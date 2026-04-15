e52b8da9557f59f2145d2811f591064c4d6032cb
N S <sibnie@web.de>
2026-04-15 12:33:30 +0200

    agent_parser fix

--- bot/agent_system_parser.py
+++ bot/agent_system_parser.py
@@ -1,3 +1,4 @@
+# agent_system_parser.py
 from __future__ import annotations
 import json
 import logging
@@ -7,7 +8,7 @@ from croniter import croniter
 import asyncpg
 from bot import brain, memory
 from bot.agent_parser import _classify_work_capability, _generate_pipeline
-from bot.models import CAPABILITY_BALANCED, CAPABILITY_DEEP_REASONING
+from bot.models import CAPABILITY_CHAT, CAPABILITY_DEEP_REASONING
 from bot.utils import clean_llm_json
 
 logger = logging.getLogger(__name__)
